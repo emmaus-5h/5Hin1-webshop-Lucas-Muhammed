a52df12de7f4c61e939b0bab26b4f9ea5affb574
lucas de haas <74540179+Lucasdehaas@users.noreply.github.com>
2022-02-17 12:03:38 +0000

    Extra tabel

--- db/create.sql
+++ db/create.sql
@@ -8,8 +8,13 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2)
+  rating_id INTEGER
 );
 
+CREATE TABLE ratings (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255)
+);
 
 --
 -- populate with data
@@ -27,3 +32,4 @@ insert into products (name, description, code, price) values ('Baklava Roze', 'D
 insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
 insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
 
+insert into ratings (name) values ('Het is lekker');
