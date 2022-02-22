8d2dfbfc2c875c29b85686f52e668d7a2c0a2c5a
Muhammed Güvec <74650757+muhammedguvec@users.noreply.github.com>
2022-02-22 10:27:24 +0000

    namen, prijs en descriptie van de baklava

--- db/create.sql
+++ db/create.sql
@@ -10,6 +10,7 @@ CREATE TABLE products (
   price NUMERIC(10, 2)
 );
 
+CREATE TABLE 
 
 --
 -- populate with data
@@ -20,10 +21,11 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Baklava Pistache', 'Het is lekker', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Baklava Walnoot', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Baklava Chocolade', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Baklava Roze', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+
+insert into products (name, description, code, price) values ('Baklava Choco', 'Baklava maar dan met chocolade voor de mensen die en chocolade en baklava lekker vinden.', '816905633-0', 20);
+insert into products (name, description, code, price) values ('Baklava Walnoot', 'Baklava maar dan met walnoten in plaats van pistashe.', '077030122-3', 21);
+insert into products (name, description, code, price) values ('Baklava Pistashe', 'Het meeste bekende van alle.', '445924201-X', 24.50);
+insert into products (name, description, code, price) values ('Baklava Sarma', 'Baklava zoals geen ander, van de turkse stad antep. Gemaakt met pistashe wrap', '693155505-7', 13.5);
+insert into products (name, description, code, price) values ('Şöbiyet', 'baklava met een lekkere romige room.', '686928463-6', 30);
+insert into products (name, description, code, price) values ('Havuç dilimi ', 'Een sensatie om van te genieten met je partner of familie op een mooie vrijdag avond.', '492662523-7', 19.50);
 
