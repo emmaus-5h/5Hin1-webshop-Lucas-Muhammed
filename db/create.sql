fa5057f1efdfc4d9f5ad933ee293a35cfc980033
lucas de haas <74540179+Lucasdehaas@users.noreply.github.com>
2022-03-06 22:33:26 +0000

    leveringstijden veranderd

--- db/create.sql
+++ db/create.sql
@@ -44,19 +44,19 @@ CREATE TABLE opslag (
 --
 
 
-insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Baklava Choco', 'Baklava maar dan met chocolade voor de mensen die en chocolade en baklava lekker vinden.', 20, 1, 2, 4, 1);
+insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Baklava Choco', 'Baklava maar dan met chocolade voor de mensen die en chocolade en baklava lekker vinden.', 20, 1, 5, 4, 1);
 insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Baklava Walnoot', 'Baklava maar dan met walnoten in plaats van pistashe.', 21, 2, 1, 1, 3);
 insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Baklava Pistashe', 'Het meeste bekende van alle.', 24.50, 3, 2, 2, 3);
 insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Baklava Sarma', 'Baklava zoals geen ander, van de turkse stad antep. Gemaakt met pistashe wrap', 13.5, 2, 3, 4, 3);
 insert into products (name, description,  price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Şöbiyet', 'baklava met een lekkere romige room.', 30, 3, 2, 1, 2);
 insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Havuç Dilimi', 'Een sensatie om van te genieten met je partner of familie op een mooie vrijdag avond.', 19.50, 2, 1, 4, 3);
-insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Dürüm Baklava', 'Dürüm baklava met pistache', 20.50, 3, 1, 2, 3);
+insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Dürüm Baklava', 'Dürüm baklava met pistache', 20.50, 3, 2, 2, 3);
   
 insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Kokos Vezir', 'Geen pistache, kokos baklava 1e graads kwaliteit dagverse baklava.', 15.50, 2, 1, 3, 1); 
   
-insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Kokos Bülbül','Baklava met kokos in een bijzonder vorm', 21.99, 2, 1, 2, 1);  
+insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Kokos Bülbül','Baklava met kokos in een bijzonder vorm', 21.99, 2, 4, 2, 1);  
   
-insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Driehoek Baklava', 'Driehoek Baklava met Pistache heeft, zoals de naam al doet vermoeden, de vorm van een driehoek. Het wordt meestal geserveerd met ijs ertussen.', 25.99, 2, 1, 2, 3);   
+insert into products (name, description, price, ratings_id, levertijd_id, opslag_id, allergie_id) values ('Driehoek Baklava', 'Driehoek Baklava met Pistache heeft, zoals de naam al doet vermoeden, de vorm van een driehoek. Het wordt meestal geserveerd met ijs ertussen.', 25.99, 2, 5, 2, 3);   
   
   
 insert into ratings (name) values ('3 van de 5 ⭐');
