43a6f564232f25d930d714b87bc5f5a8a7a5d756
lucas de haas <74540179+Lucasdehaas@users.noreply.github.com>
2022-03-06 13:44:03 +0000

    api en table van levering

--- db/create.sql
+++ db/create.sql
@@ -8,7 +8,8 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2),
-  ratings_id INTEGER 
+  ratings_id INTEGER, 
+  levering_id INTEGER 
 );
 
 
@@ -16,7 +17,16 @@ CREATE TABLE ratings (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255) 
 );
---
+
+CREATE TABLE levering (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  leveringstijd VARCHAR(255) 
+);
+  
+  
+  
+  
+  --
 -- populate with data
 --
 -- generared using
@@ -26,13 +36,18 @@ CREATE TABLE ratings (
 --
 
 
-insert into products (name, description, code, price, ratings_id) values ('Baklava Choco', 'Baklava maar dan met chocolade voor de mensen die en chocolade en baklava lekker vinden.', '816905633-0', 20, 1);
-insert into products (name, description, code, price, ratings_id) values ('Baklava Walnoot', 'Baklava maar dan met walnoten in plaats van pistashe.', '077030122-3', 21, 2);
-insert into products (name, description, code, price, ratings_id) values ('Baklava Pistashe', 'Het meeste bekende van alle.', '445924201-X', 24.50, 3);
-insert into products (name, description, code, price, ratings_id) values ('Baklava Sarma', 'Baklava zoals geen ander, van de turkse stad antep. Gemaakt met pistashe wrap', '693155505-7', 13.5, 2);
-insert into products (name, description, code, price, ratings_id) values ('Şöbiyet', 'baklava met een lekkere romige room.', '686928463-6', 30, 3);
-insert into products (name, description, code, price, ratings_id) values ('Havuç dilimi', 'Een sensatie om van te genieten met je partner of familie op een mooie vrijdag avond.', '492662523-7', 19.50, 2);
+insert into products (name, description, code, price, ratings_id, levering_id) values ('Baklava Choco', 'Baklava maar dan met chocolade voor de mensen die en chocolade en baklava lekker vinden.', '816905633-0', 20, 1, 2);
+insert into products (name, description, code, price, ratings_id, levering_id) values ('Baklava Walnoot', 'Baklava maar dan met walnoten in plaats van pistashe.', '077030122-3', 21, 2, 1);
+insert into products (name, description, code, price, ratings_id, levering_id) values ('Baklava Pistashe', 'Het meeste bekende van alle.', '445924201-X', 24.50, 3, 2);
+insert into products (name, description, code, price, ratings_id, levering_id) values ('Baklava Sarma', 'Baklava zoals geen ander, van de turkse stad antep. Gemaakt met pistashe wrap', '693155505-7', 13.5, 2, 3);
+insert into products (name, description, code, price, ratings_id, levering_id) values ('Şöbiyet', 'baklava met een lekkere romige room.', '686928463-6', 30, 3, 2);
+insert into products (name, description, code, price, ratings_id, levering_id) values ('Havuç dilimi', 'Een sensatie om van te genieten met je partner of familie op een mooie vrijdag avond.', '492662523-7', 19.50, 2, 1);
 
 insert into ratings (name) values ('Geweldig');
 insert into ratings (name) values ('Super lekker');
 insert into ratings (name) values ('Het is goddelijk');
+
+insert into levering (leveringstijd) values ('1 week');
+insert into levering (leveringstijd) values ('2 dagen');
+insert into levering (leveringstijd) values ('1 dag');
+
